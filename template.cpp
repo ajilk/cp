20b8ad202c95a61f856708ede189f89390d5c216
ajilk <ajilkhomov@gmail.com>
2020-05-23 13:16:52 -0400

    Create a template starter file

---
+++ template.cpp
@@ -0,0 +1,31 @@
+/* <title>
+ *
+ * <link>
+ *
+ * <notes>
+ *
+ */
+
+#define RESET "\033[0m"
+#define RED "\033[31m"
+#define DEBUG(x) cout << RED << "\n==> " << RESET << #x << ": " << x;
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <class T>
+void print(T* arr, int size) {
+  for (int i = 0; i < size; i++) cout << arr[i];
+}
+
+template <class T>
+void print(vector<T>& a) {
+  for (int i = 0; i < a.size(); i++) cout << a.at(i) << " ";
+  cout << endl;
+}
+
+int main() {
+  /* code */
+  return 0;
+}
