5617b93286c68d0b5a4f98def02045b14690b55c
ajilk <ajilkhomov@gmail.com>
2021-06-18 01:37:38 -0400

    Add speed trick

--- _template_.cpp
+++ _template_.cpp
@@ -5,6 +5,11 @@
  * <notes>
  *
  */
+static const auto speedup = []() {
+    std::ios::sync_with_stdio(false); 
+    std::cin.tie(nullptr); 
+    return 0;
+}();
 
 #define fr(i, n) for (int i = 0; i < (n); ++i)
 
